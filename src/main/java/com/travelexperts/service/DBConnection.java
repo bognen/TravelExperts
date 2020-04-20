0054f9e055b7bc2ef832a3f6ccb6ab412674623d
Dima Bognen <34019262+bognen@users.noreply.github.com>
2020-04-20 15:26:29 -0600

    Remove passwd

--- src/main/java/com/travelexperts/service/DBConnection.java
+++ src/main/java/com/travelexperts/service/DBConnection.java
@@ -1,6 +1,6 @@
 //********************************************//
 // Dima Bognen, Jonathan Pirca, Abel Rojas, Manish Sudani
-// Service whichprovides DB connection  
+// Service whichprovides DB connection
 //********************************************//
 
 package com.travelexperts.service;
@@ -9,7 +9,7 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 
 public class DBConnection {
-	
+
 	public static Connection getConnection() {
 		Connection conn = null;
 
@@ -18,7 +18,7 @@ public class DBConnection {
             Class.forName("com.mysql.cj.jdbc.Driver");
 			//String myUrl = "jdbc:mysql://travelexperts.cc3i7aicdfky.us-east-2.rds.amazonaws.com:3306/travelexperts";
 			String myUrl = "jdbc:mysql://localhost:3306/travelexperts";
-			conn = DriverManager.getConnection(myUrl, "root", "39PGR6umu9TU");
+			conn = DriverManager.getConnection(myUrl, "****", "***********");
 			return conn;
 			} catch (Exception e) {
 	        System.err.println("Got an exception! ");
